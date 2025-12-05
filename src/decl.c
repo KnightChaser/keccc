3380ecce23086b24da6a52005df0af220a9815e9
knightchaser <agerio100@naver.com>
2025-12-05 23:40:16 +0900

    Docs: Add missing function documentation at decl.c

--- src/decl.c
+++ src/decl.c
@@ -4,9 +4,17 @@
 #include "data.h"
 #include "defs.h"
 
+/**
+ * variableDeclaration - Parses a variable declaration.
+ *
+ * NOTE:
+ * Currently, we only support integer variable declarations.
+ * Thus, we ensure that the type is 'int', followed by an identifier
+ * and a semicolon(;).
+ */
 void variableDeclaration(void) {
     match(T_INT, "int");
-    identifier();
+    identifier(); // Text now has the identifier's name
     addGlobalSymbol(Text);
     codegenDeclareGlobalSymbol(Text);
     semicolon();
